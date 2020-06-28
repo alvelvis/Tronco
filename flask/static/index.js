6a4d87fff29bd36490182a432545ad5828eb3ec0
Elvis de Souza <elvis.desouza99@gmail.com>
2020-06-28 10:45:18 -0300

    update

--- flask/static/index.js
+++ flask/static/index.js
@@ -39,8 +39,6 @@ $(window).bind("keyup", function(){
     }
 })
 
-var expirationDate = 'Fri, 31 Dec 9999 23:59:59 GMT'
-
 function addRecent (newName){
     recent = getRecent()
     newRecent = []
