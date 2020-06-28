a6ba997be01c90c3b3b06289bccea24234a98242
Elvis de Souza <elvis.desouza99@gmail.com>
2020-06-28 10:19:54 -0300

    update

--- flask/static/tronco.js
+++ flask/static/tronco.js
@@ -5,23 +5,3 @@ document.addEventListener('DOMContentLoaded', function() {
     setInterval(function(){ fetch(url, { mode: 'no-cors'});}, 5000);//();
 
 });
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent the mini-infobar from appearing on mobile
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can install the PWA
-    showInstallPromotion();
-});
-
-$('#downloadTronco').click(function(e){
-    if (isMobile) {
-        e.preventDefault()
-        // Hide the app provided install promotion
-        hideMyInstallPromotion();
-        // Show the install prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-    };
-})
