206117f2bc5971dd84b5c50441a7dfdca964ee0c
Elvis de Souza <elvis.desouza99@gmail.com>
2020-07-07 18:41:53 -0300

    update

--- flask/objects/__init__.py
+++ flask/objects/__init__.py
@@ -14,8 +14,8 @@ tronco_metadata = ["last_seen", "first_seen", "times_seen"]
 root_path = ""
 
 udpipe_models = {
-    'pt': os.path.join(root_path, "udpipe", "portuguese-bosque-ud-2.5-191206.udpipe"),
-    'en': os.path.join(root_path, "udpipe", "english-ewt-ud-2.5-191206.udpipe")
+    'pt': "portuguese-bosque-ud-2.5-191206.udpipe",
+    'en': "english-ewt-ud-2.5-191206.udpipe"
 }
 
 all_permissions = ["visualizar", "editar", "configurar"]
@@ -104,9 +104,9 @@ class AdvancedCorpora:
             del self.corpora[name]
         corpus_dir = os.path.join(root_path, "corpora", name)
         corpus_language = lang
-        model = Model.load(udpipe_models[corpus_language])
-        self.models[corpus_language] = model
-        pipeline = Pipeline(self.models[corpus_language], "tokenize", Pipeline.DEFAULT, Pipeline.DEFAULT, "conllu")
+        model = Model.load(os.path.join(root_path, "udpipe", udpipe_models[corpus_language]))
+        sys.stderr.write(os.path.join(root_path, "udpipe", udpipe_models[corpus_language]))
+        pipeline = Pipeline(model, "tokenize", Pipeline.DEFAULT, Pipeline.DEFAULT, "conllu")
         corpus = estrutura_ud.Corpus(recursivo=True)
         all_metadata = {'filename': ''}
         for filename in os.listdir(corpus_dir):
@@ -148,7 +148,6 @@ class AdvancedCorpora:
 
     def __init__(self):
         self.corpora = {}
-        self.models = {}
         self.config_file = os.path.join(root_path, "advanced_corpora.p")
         if os.path.isfile(self.config_file):
             with open(self.config_file, "rb") as f:
--- flask/wsgi.py
+++ flask/wsgi.py
@@ -1,9 +1,8 @@
-activate_this = '/var/www/Tronco/flask/uvenv/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
-import sys
-import time, os
+import sys, time, os
+if os.path.isdir("/var/www/Tronco"):
+	activate_this = '/var/www/Tronco/flask/uvenv/bin/activate_this.py'
+	with open(activate_this) as file_:
+	    exec(file_.read(), dict(__file__=activate_this))
 
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
