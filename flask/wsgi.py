53085e7ecb086dc49760df627339af907e56d341
Elvis de Souza <elvis.desouza99@gmail.com>
2020-07-07 15:56:23 -0300

    update

--- flask/wsgi.py
+++ flask/wsgi.py
@@ -1,11 +1,12 @@
+activate_this = '/var/www/Tronco/flask/uvenv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
 import sys
 import time, os
 
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
 sys.path.insert(0, "/var/www/Tronco/flask")
-import site
-
-site.addsitedir('uvenv/lib/python3.6/site-packages')
 
 from app import app as application
