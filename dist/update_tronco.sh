455e3b3676e9719a294920cfc7c4d1c1e440cb75
Elvis de Souza <elvis.desouza99@gmail.com>
2020-07-08 08:08:57 -0300

    update

--- dist/update_tronco.sh
+++ dist/update_tronco.sh
@@ -1,4 +1,4 @@
-sed -r 's/(.*)(\?version=)([0-9]+)(.*)/echo "\1\2$((\3+1))\4"/ge' ../flask/templates/dashboard.html
-sed -r 's/(.*)(\?version=)([0-9]+)(.*)/echo "\1\2$((\3+1))\4"/ge' ../flask/templates/index.html
+sed -r 's/(.*)(\?version=)([0-9]+)(.*)/echo "\1\2$((\3+1))\4"/ge' ../flask/templates/dashboard.html > 1
+sed -r 's/(.*)(\?version=)([0-9]+)(.*)/echo "\1\2$((\3+1))\4"/ge' ../flask/templates/index.html > 2
 git add -u; git commit -m "update"; git push
 gcloud compute ssh tronco -- "cd /var/www/Tronco && sudo git pull && sudo service apache2 restart"
