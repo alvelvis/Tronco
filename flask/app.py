34c1ed00ffeb135ec8a3cbec348b0378ad09cc51
Elvis de Souza <elvis.desouza99@gmail.com>
2020-07-12 03:15:45 -0300

    update

--- flask/app.py
+++ flask/app.py
@@ -4,7 +4,6 @@ import random
 import json
 from flask import Flask, redirect, render_template, request, url_for, send_from_directory
 #from webui import WebUI
-from pyfladesk import init_gui
 #from flaskwebgui import FlaskUI
 from uuid import uuid4
 
@@ -439,5 +438,6 @@ def home():
         )
 
 if __name__ == "__main__":
+    from pyfladesk import init_gui
     init_gui(app, port=5240, width=1024, height=768,
              window_title="Tronco", icon=os.path.join(app.root_path, "static", "favicon.png"), argv=None)#ui.run()
