033432ce38601c1165771deddaf39da0cf16de9f
Elvis de Souza <elvis.desouza99@gmail.com>
2020-07-13 11:05:55 -0300

    update

--- flask/app.py
+++ flask/app.py
@@ -444,5 +444,5 @@ def home():
 
 if __name__ == "__main__":
     from pyfladesk import init_gui
-    init_gui(app, port=5240, width=1280, height=768,
-             window_title="Tronco", icon=os.path.join(app.root_path, "static", "favicon.png"), argv=None)#ui.run()
+    init_gui(app, port=5240, width=800, height=600,
+             window_title="Tronco", icon=os.path.join(app.root_path, "static", "favicon.png"))
