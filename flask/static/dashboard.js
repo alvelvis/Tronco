1181a71e3231dff0135f5a9b405f6962cd4b9cff
Elvis de Souza <elvis.desouza99@gmail.com>
2020-10-23 13:04:01 -0300

    update

--- flask/static/dashboard.js
+++ flask/static/dashboard.js
@@ -2071,10 +2071,11 @@ function triggerResize(first=false){
                 $('#mobileEdit').css({top: window.innerHeight-75, left: (window.innerWidth/2)-28})
             })
             $('#filename, #filename-div').hide()
+            //$('#troncoHomeLabel').css('overflow-x', 'scroll').css('white-space', 'nowrap')
         }
         isMobile = true
         filename = $('#filename').attr('file')
-        $('#troncoHomeLabel').html("<span class='mt-3 mb-2' style='max-width:90vw; width:100%; display:inline-block; white-space: nowrap; overflow:hidden; font-weight:bold; text-overflow:ellipsis'>" + name + " > <span id='filenameMobile'>" + (special_files.indexOf(filename) == -1 ? filename : "") + "</span>" + "</span>")
+        $('#troncoHomeLabel').html("<span class='mt-3 mb-2 ml-3' style='overflow-x: scroll; max-width:90%; width:100%; display:inline-block; white-space: nowrap; font-weight:bold;'>" + name + " > <span id='filenameMobile'>" + (special_files.indexOf(filename) == -1 ? filename : "") + "</span>" + "</span>")
         $('#troncoLogo').hide()
         $('.navbar-brand').hide()
         $('#toolbar-group, #searchHeader, .dynamic, [advanced-toolbar-panel!="builder"].advanced-toolbar-panel, [advanced-toolbar-panel!="builder"] .h5, [advanced-toolbar-panel="builder"] .btn-group, #toolbar, #advancedSearchToolbarRow .btn-group, #filename, #saved, #breadcrumb-nav, #mainText, #hr').toggleClass("px-5", false).toggleClass("px-4", true)
@@ -2111,7 +2112,7 @@ function triggerResize(first=false){
         $('#search').show()
     }
     $('#troncoHomeBar').css("width", (isMobile ? "100%" : ""))
-    $('#troncoHomeBar').toggleClass("mt-0", isMobile).toggleClass("ml-3", isMobile)
+    $('#troncoHomeBar').toggleClass("mt-0", isMobile)//.toggleClass("ml-3", isMobile)
     $('#sidebar').css('margin-top', $('#sidebar').css('top') == "0px" ? (isMobile ? "50px" : '54px') : '10px')
     $('#troncoLogo').css('margin-bottom', isMobile ? "" : "4px")
     //$('#main').css('margin-left', !isMobile ? '260px' : '0px')
