d04d6c6a45f023e41dbbb33dae2187871aa486ce
Elvis de Souza <elvis.desouza99@gmail.com>
2021-07-18 21:01:57 -0300

    update

--- flask/static/dashboard.js
+++ flask/static/dashboard.js
@@ -1895,6 +1895,19 @@ $(window).bind('keydown', function(event) {
     }
 })
 
+var textareas = document.getElementsByTagName('textarea');
+var count = textareas.length;
+for(var i=0;i<count;i++){
+    textareas[i].onkeydown = function(e){
+        if(e.keyCode==9 || e.which==9){
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value = this.value.substring(0,this.selectionStart) + "    " + this.value.substring(this.selectionEnd);
+            this.selectionEnd = s+4; 
+        }
+    }
+}
+
 var failedSave = false
 
 function saveFile(filename=$('#filename').attr('file'), text=$('#mainText').val()){
