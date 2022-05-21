ef36f78f5369dd5b102d261a3493171ddf8fd849
Elvis de Souza <elvis.desouza99@gmail.com>
2022-05-21 18:57:08 -0300

    update

--- flask/static/dashboard.js
+++ flask/static/dashboard.js
@@ -3,14 +3,14 @@ function countWords(str) {
 }
 
 $('#search').on('blur', function(){
-    /*setTimeout(function(){
+    setTimeout(function(){
         if (isMobile) {
             $('#search').hide()
         }
         if ($('.filename').attr('file') != "README" || $('#recentFiles').text() == 'Nenhum arquivo encontrado.') { $('#breadcrumb-nav').hide() }
-    }, 500)*/
-    $('#search').fadeOut()
-    if ($('.filename').attr('file') != "README" || $('#recentFiles').text() == 'Nenhum arquivo encontrado.') { $('#breadcrumb-nav').fadeOut() }
+    }, 500)
+    //$('#search').fadeOut()
+    //if ($('.filename').attr('file') != "README" || $('#recentFiles').text() == 'Nenhum arquivo encontrado.') { $('#breadcrumb-nav').fadeOut() }
     $(fade_on_search_focus).fadeIn()
 })
 
